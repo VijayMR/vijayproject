1773072e3b0e7689e9991f84e3c8aff8ffc4fbfb
VijayMR <vijayragupathy23@gmail.com>
2021-07-27 21:43:26 +0530

    task

---
+++ src/org/count/Evencount.java
@@ -0,0 +1,15 @@
+package org.count;
+
+public class Evencount {
+	public static void main(String[] args) {
+		int j=0;
+		for (int i = 1; i <= 100; i++) {
+			if(i%2==0) {
+				j=j+1;
+			}
+			
+		}
+		System.out.println("Count of even number is "+j);
+	}
+
+}
